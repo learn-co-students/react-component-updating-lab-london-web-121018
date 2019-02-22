037f8d03bf8e2a4a0573d383b9027463bc3a57d2
github username <github email address>
2019-02-22 13:20:31 +0000

    learn submit

--- src/Timer.js
+++ src/Timer.js
@@ -11,9 +11,21 @@ class Timer extends Component {
     }
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+
 
 
   //Your code here
+
+  componentDidUpdate () {
+    return this.timer.current.style.color =
+      '#' + Math.floor(Math.random() * 16777215).toString(16)
+  }
 
 
 
